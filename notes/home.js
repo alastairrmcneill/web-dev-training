549e4e72dba995d1a27e4b2120159c68606379a8
Alastair McNeill <alastair.r.mcneill@gmail.com>
2023-10-09 22:24:22 +0100

    Notes: small tweaks

---
+++ notes/home.js
@@ -0,0 +1,7 @@
+// Get a reference to the icon element by its ID
+const saveButton = document.getElementById("save-button");
+
+saveButton.addEventListener("click", function () {
+    console.log("Save")
+});
+
